d7e7f243eee9f63196ccf2f1adcd7376b4930e7a
chaji0 <jiyoungc006@gmail.com>
2026-08-20 03:11:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * ★ 교과서 내용을 수정해서 새로 올릴 때는 아래 VERSION 숫자를 반드시 하나 올려 주세요.
  *   (예: v1.0.0 → v1.0.1)  이 숫자가 바뀌어야 학생들의 앱이 새 버전을 받아옵니다.
  */
-var VERSION = "v1.0.1";
+var VERSION = "v1.0.2";
 
 var CACHE = "moving-calc-" + VERSION;
 var ASSETS = [
